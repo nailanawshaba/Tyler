6f01c758d5ca423149ae09dd73094fc8e78b3f44
pierreca <pierre.cauchois@gmail.com>
2013-09-25 06:50:31 +0000

    fixing stupid bugs

--- SonosConnect/SonosConnect/SonosConnect.py
+++ SonosConnect/SonosConnect/SonosConnect.py
@@ -32,7 +32,7 @@ def send_to_peggy(text):
 def is_same_song(song1, song2):
     if song1 == None or song2 == None:
         return False
-
+    
     if song1['artist'] == song2['artist'] and song1['title'] == song2['title'] and song1['album'] == song2['album']:
         return True
 
@@ -47,15 +47,16 @@ def run():
         zone_name = device.get_speaker_info()['zone_name']
         print "IP of {0} is {1}".format(zone_name, ip)
            
-        now_playing = device.get_current_track_info();  
+        now_playing = None
 
         while True:
             try:
                 tmp = device.get_current_track_info()
                 if not is_same_song(now_playing, tmp):     
+                    now_playing = tmp
                     print "Now Playing: {0} - {1} ({2})".format(now_playing['artist'], now_playing['title'], now_playing['album'])
                     send_to_peggy("{0}:{1}".format(now_playing['artist'], now_playing['title']))
-                    sleep(20)
+                sleep(20)
             except KeyboardInterrupt:
                 break
     

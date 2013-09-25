4311d8370ffbcca394e595f1549e97cfe2536ff9
pierreca <pierre.cauchois@gmail.com>
2013-09-24 23:33:29 -0700

    Sending new Song title to Peggy board only when song title actually changed.

--- SonosConnect/SonosConnect/SonosConnect.py
+++ SonosConnect/SonosConnect/SonosConnect.py
@@ -28,6 +28,17 @@ def send_to_peggy(text):
         ser.close()
         return False
 
+
+def is_same_song(song1, song2):
+    if song1 == None or song2 == None:
+        return False
+
+    if song1['artist'] == song2['artist'] and song1['title'] == song2['title'] and song1['album'] == song2['album']:
+        return True
+
+    return False
+
+
 def run():
     ips = find_sonos()
     print "Found {0} device(s)".format(len(ips))
@@ -36,12 +47,15 @@ def run():
         zone_name = device.get_speaker_info()['zone_name']
         print "IP of {0} is {1}".format(zone_name, ip)
            
+        now_playing = device.get_current_track_info();  
+
         while True:
-            try:     
-                now_playing = device.get_current_track_info()
-                print "Now Playing: {0} - {1} ({2})".format(now_playing['artist'], now_playing['title'], now_playing['album'])
-                send_to_peggy("{0}:{1}".format(now_playing['artist'], now_playing['title']))
-                sleep(20)
+            try:
+                tmp = device.get_current_track_info()
+                if not isSameSong(now_playing, tmp):     
+                    print "Now Playing: {0} - {1} ({2})".format(now_playing['artist'], now_playing['title'], now_playing['album'])
+                    send_to_peggy("{0}:{1}".format(now_playing['artist'], now_playing['title']))
+                    sleep(20)
             except KeyboardInterrupt:
                 break
     
